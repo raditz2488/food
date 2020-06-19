c7f6081f9281903a94fbbf023264d126dc334426
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-19 15:29:24 +0530

    SearchBar uses the term and onTermChange props to set the value of TextInput and onTermChange to set value of term state of SearchScreen.
    Update:
    1. Destructure prop into term and onTermChange for the SearchBar component.
    2. In TextInput of SearchBar the value prop is set to term and onChangeText is set to onTermChange.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
     return <View style={styles.backgroundStyle}>
         <Feather name='search' style={styles.iconStyle}/>
-        <TextInput placeholder='Search' style={styles.inputStyle}/>
+        <TextInput 
+            placeholder='Search' 
+            style={styles.inputStyle}
+            value={term}
+            onChangeText={(onTermChange}
+        />
     </View>;
 }
 
