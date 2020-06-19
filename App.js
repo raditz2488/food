75bb0fd225f8475d93fff18aae6068278d6d3e9e
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-19 13:44:39 +0530

    Added SearchScreen and set it as initial route.
    Update:
    1. Created SearchScreen.js which declares SearchScreen component. It displays a Text sample currently.
    2. App.js imports the SearchScreen and sets it as the initial route.

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
 const navigator = createStackNavigator(
   {
-
+    Search: SearchScreen
   },
   {
-
+    initialRouteName: 'Search'
   }
 )
 
