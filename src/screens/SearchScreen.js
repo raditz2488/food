9a82d04d6f768a459c946e2cee3c883028a8fa59
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-19 14:42:49 +0530

    Added state named term to the SearchScreen.
    Update:
    1. Imported useState hook inside SearchScreen.js.
    2. Created a state named term and setter named setTerm using useState hook.
    3. Added term and onTermChange property on the Text component.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () => {
+    const [term, setTerm] = useState('')
     return <View>
         <SearchBar/>
-        <Text>Search Screen</Text>
+        <Text term={term} onTermChange={(newTerm) => setTerm(newTerm)}>Search Screen</Text>
     </View>;
 }
 
