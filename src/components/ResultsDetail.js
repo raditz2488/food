d96acb4bdb739b0a19dc47c9c98fb81080f6bcec
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 22:45:57 +0530

    Covered the Image and Text components inside ResultsDetail with TouchableOpacity.

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const ResultsDetail = ({result}) => {
     return <View style={styles.container}>
-        <Image style={styles.imageStyle} source={{ uri: result.image_url }}/>
-        <Text style={styles.nameStyle}>{result.name}</Text>
-        <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
+        <TouchableOpacity>
+            <Image style={styles.imageStyle} source={{ uri: result.image_url }}/>
+            <Text style={styles.nameStyle}>{result.name}</Text>
+            <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
+        </TouchableOpacity>
     </View>;
 }
 
