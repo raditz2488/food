520dc3d6f4fb8b822df955f88f74c18698f2ad2f
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 19:53:23 +0530

    Added marginLeft of 15 to the ResultsDetail component

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ResultsDetail = ({result}) => {
-    return <View>
+    return <View style={styles.container}>
         <Image style={styles.imageStyle} source={{ uri: result.image_url }}/>
         <Text style={styles.nameStyle}>{result.name}</Text>
+        <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>;
 }
 
@@ -17,6 +18,9 @@ let styles = StyleSheet.create(
         },
         nameStyle: {
             fontWeight: 'bold'
+        },
+        container: {
+            marginLeft: 15
         }
     }
 );
