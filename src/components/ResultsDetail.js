08444f616379df9b29ee57fe637da6703b0c0f84
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 19:03:16 +0530

    Created barebones implementation for ResultsDetail component.

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsDetail = () => {
+    return <View>ResultsDetail</View>
+}
+
+let styles = StyleSheet.create({});
+
+export default ResultsDetail;
