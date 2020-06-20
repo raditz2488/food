37f55b992a9d6a81b622efc3df71ee53a57e7dd6
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 19:23:20 +0530

    Added Image component to the ResultsDetail to display image of the Hotels.

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ResultsDetail = ({result}) => {
     return <View>
+        <Image source={{ uri: result.image_url }}/>
         <Text>{result.name}</Text>
     </View>;
 }
