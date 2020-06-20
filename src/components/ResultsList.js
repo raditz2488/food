a61bf7b88e0642526dca7a5ceeb110740cd9b2ab
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 18:37:23 +0530

    Added horizontal FlatList component to display barebone version of result names inside ResultsList component.

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ResultsList = ({ title, results }) => {
     return <View>
         <Text style={styles.titleStyle}>{title}</Text>
-        <Text>Results {results.length}</Text>
+        <FlatList
+            horizontal
+            data={results}
+            keyExtractor={ (result) => result.id }
+            renderItem={ ({ item }) => <Text>{item.name}</Text>  }
+        />
     </View>
 }
 
