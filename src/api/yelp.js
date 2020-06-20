f982d03f7fbbe156bd0f797f6cc582dded18cabe
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 15:13:28 +0530

    Installed axios to the project.
    Update:
    1. Installed axios using npm.
    2. Created yelp api using axios.
    3. Imported yelp into SearchScreen.

---
+++ src/api/yelp.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default axios.create(
+    {
+        baseURL: "https://api.yelp.com/v3/businesses",
+        headers: {
+            Authorization: "Bearer 3fuSgfsW1NM7NgHrctKJY24Kfk1DId0RSPPgad-Q-UE3flBl00E2tI9xapdimxYgjsWfmwWLIS3n1cNqHhbFvlD4XUSh1O6nspEAMJh_eceRVpnEQhZBK8Ejpy3sXnYx"
+        }
+    }
+)
