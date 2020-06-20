b8a920fb7e110759587db7e2f15ee1a8791ddc86
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 15:31:39 +0530

    Added a function to call search on yelp.
    Update:
    1. Added function named searchAPI which calls yelp api to get searches for a term. This function sets the results state which in turn updates the ui displaying the results count.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,12 +6,24 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
-    
+
+    const searchAPI = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term,
+                location: 'san jose'
+            }
+        });
+
+        setResults(response.data.businesses);
+    }
+
     return <View>
         <SearchBar 
             term={term} 
             onChangeTerm={(newTerm) => setTerm(newTerm)}
-            onTermSubmit={ () => console.log('term is submitted') }    
+            onTermSubmit={ () => searchAPI() }    
         />
         <Text>Found {results.length} results.</Text>
     </View>;
