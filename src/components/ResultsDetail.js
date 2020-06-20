6b60fe629eef499faa5cff3f72be59eef5b7bcbe
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 19:25:16 +0530

    Added styling to the Image component inside the ResultsDetail component.

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,11 +3,19 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ResultsDetail = ({result}) => {
     return <View>
-        <Image source={{ uri: result.image_url }}/>
+        <Image style={styles.imageStyle} source={{ uri: result.image_url }}/>
         <Text>{result.name}</Text>
     </View>;
 }
 
-let styles = StyleSheet.create({});
+let styles = StyleSheet.create(
+    {
+        imageStyle: {
+            width: 250,
+            height: 120,
+            borderRadius: 4
+        }
+    }
+);
 
 export default ResultsDetail;
