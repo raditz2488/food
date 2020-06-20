302b6fdafd79bae2927374fdf6908849fd99b0b3
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 18:09:21 +0530

    Extracted Search API logic into useResults hook.
    Update:
    1. Moved the code containing stuff related to API call to the useResults.js and as a result created custom hook.
    2. Used this hook inside SearchScreen to call api for search businesses from yelp.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,40 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const searchAPI = async (searchTerm) => {
-        console.log(searchTerm)
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            });
     
-            setResults(response.data.businesses);
-            setErrorMessage("");
-        } catch(err) {
-            setErrorMessage("Something went wrong.");
-        }
-        
-        
-    }
+    const [searchAPI, results, errorMessage] = useResults();
 
-    // BAD CODE
-    useEffect(
-        () => {
-            searchAPI('pasta')
-        },
-        []
-    )
 
     return <View>
         <SearchBar 
