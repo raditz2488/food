9da76852eeaf54618438c47b861e0900956e97f1
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-06-20 22:57:16 +0530

    Used withNavigation to wrap ResultsList so that it can receive navigation object as a prop.

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import ResultsDetail from './ResultsDetail';
+import { withNavigation } from 'react-navigation';
 
 const ResultsList = ({ title, results }) => {
     return <View style={styles.container}>
@@ -36,4 +37,4 @@ const styles = StyleSheet.create(
     }
 );
 
-export default ResultsList;
+export default withNavigation(ResultsList);
